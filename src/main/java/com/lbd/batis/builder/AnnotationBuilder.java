dd766c2f6c78af032af8b9f3345f754c3b8bd006
hanzejl <hanzejl@gmail.com>
2020-04-15 15:32:49 +0000

    add annotations and builder

---
+++ src/main/java/com/lbd/batis/builder/AnnotationBuilder.java
@@ -0,0 +1,5 @@
+package com.lbd.batis.builder;
+
+
+public class AnnotationBuilder extends BaseBuilder {
+}
---
+++ src/main/java/com/lbd/batis/builder/BaseBuilder.java
@@ -0,0 +1,5 @@
+package com.lbd.batis.builder;
+
+
+public abstract class BaseBuilder {
+}
